d765140978b41717dfe6165c1385e18716698f86
Kron <narongkron1@hotmail.com>
2020-12-02 23:02:43 -0800

    edit stype

--- src/components/Kanban.vue
+++ src/components/Kanban.vue
@@ -1,5 +1,7 @@
 <template>
-  
+    <div class="Kanban">
+        Kanban
+    </div>
 </template>
 
 <script>
@@ -9,5 +11,9 @@ export default {
 </script>
 
 <style>
-
+.Kanban{
+    background-color: aqua;
+    width: 100%;
+    height: 100%;
+}
 </style>
