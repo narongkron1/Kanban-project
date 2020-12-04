f30192c216bab0d4c31e3dac183465c9a8e70ee5
Kron <narongkron1@hotmail.com>
2020-12-04 04:57:55 -0800

    เพิ่ม Drop zone

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,11 @@
 <template>
   <div id="app">
-    <Kanban :data="data"/>
-    
+    <Kanban :data="data" :create_task_submit="create_task_submit"/>
   </div>
 </template>
 
 <script>
-import Kanban from"./components/Kanban"
-
+import Kanban from "./components/Kanban"
 
 export default {
   name: 'App',
@@ -16,28 +14,46 @@ export default {
   },
   data(){
     return{
-    data:[
-            {
-                name:"todu",
-                color:"#FF99FF",
+      data:[
+        {
+          name:"todo",
+          color:"#FA8072",
+          tasks:[{
+            task_name:"test",
             },
-            {
-                name:"doing",
-                color:"#FFFF00",
+          ],
+        },
+        {
+          name:"doing",
+          color:"#6495ED",
+          tasks:[{
+            task_name:"test",
             },
-            {
-                name:"done",
-                color:"#99FFCC",
+          ],
+        },
+        {
+          name:"done",
+          color:"#FFA07A",
+          tasks:[{
+            task_name:"test",
             },
+          ],
+        }
+      ]
+    }
+  },
+  methods:{
+    create_task_submit(column_index,item){
+      this.data[column_index].tasks.push(item)
 
-        ]
+    }
   }
 }
-        }
 </script>
 
 <style>
 #app {
   height: 100vh;
+
 }
 </style>
--- src/components/Kanban.vue
+++ src/components/Kanban.vue
@@ -9,12 +9,33 @@
       <div class="column-header">
         {{ column.name }}
       </div>
-      <div class="column-boby">
-        <div class="create-task" @click="create_task(index)">create task</div>
+
+      <div class="column-body">
+        <div v-for="(task, task_index) in column.tasks" :key="task_index">
+          <div
+            class="task"
+            :draggable="true"
+            @dragstart="start_move(task_index, index)"
+          >
+            {{ task.task_name }}
+          </div>
+          <div
+            class="drop_zone"
+            @dragenter.prevent="drop_zone_enter"
+            @dragleave="drop_zone_leave"
+            @dragover.prevent
+          ></div>
+        </div>
+
+        <div class="create-task" @click="create_task(index)">Create Task</div>
       </div>
     </div>
-    <b-modal ref="create-task-modal"  title="Create Task">
-      <p class="my-4">Hello from modal!</p>
+    <b-modal ref="create-task-modal" title="Create Task">
+      <input
+        class="input-task-name"
+        v-model="task_name"
+        @keyup.13="submit_create_task"
+      />
     </b-modal>
   </div>
 </template>
@@ -23,12 +44,39 @@
 export default {
   props: {
     data: Array,
+    create_task_submit: Function,
   },
   methods: {
-    create_task(index_colunm) {
-      console.log(index_colunm)
-      this.$refs["create-task-modal"].show()
+    create_task(index_column) {
+      this.current_column_index = index_column;
+      this.$refs["create-task-modal"].show();
     },
+    submit_create_task() {
+      this.create_task_submit(this.current_column_index, {
+        task_name: this.task_name,
+      });
+    },
+    start_move(task_index, column_index) {
+      this.current_column_index = column_index;
+      this.current_task_index = task_index;
+    },
+    drop_zone_enter(event) {
+      event.target.style.height = "100px";
+      event.target.style.borderStyle = "solid";
+      event.target.style.transition = "height 0.5s";
+    },
+    drop_zone_leave(event) {
+      event.target.style.height = "10px";
+      event.target.style.borderStyle = "none";
+      event.target.style.transition = "height 0.5s";
+    },
+  },
+  data() {
+    return {
+      task_name: "",
+      current_column_index: "",
+      current_task_index: "",
+    };
   },
 };
 </script>
@@ -37,7 +85,7 @@ export default {
 .kanban {
   width: 100%;
   height: 100%;
-  background-color: rgb(136, 207, 248);
+  background-color: bisque;
 }
 .column {
   height: 600px;
@@ -46,9 +94,9 @@ export default {
   display: inline-block;
   margin: 25px;
   padding: 10px;
-  -webkit-box-shadow: 10px 10px 5px -1px rgba(0, 0, 0, 0.75);
-  -moz-box-shadow: 10px 10px 5px -1px rgba(0, 0, 0, 0.75);
-  box-shadow: 10px 10px 5px -1px rgba(0, 0, 0, 0.75);
+  -webkit-box-shadow: 0px 4px 15px 0px rgba(0, 0, 0, 0.75);
+  -moz-box-shadow: 0px 4px 15px 0px rgba(0, 0, 0, 0.75);
+  box-shadow: 0px 4px 15px 0px rgba(0, 0, 0, 0.75);
 }
 .column-header {
   display: flex;
@@ -58,11 +106,11 @@ export default {
   font-size: 32px;
   font-weight: bold;
 }
-.column-boby {
-  height: calc(100% - 50px);
+.column-body {
+  height: calc(100% - 60px);
   border-radius: 10px;
   padding: 5px;
-  background-color: rgba(143, 252, 252, 0.404);
+  background-color: #ffffff7c;
 }
 .create-task {
   width: 100%;
@@ -72,6 +120,20 @@ export default {
   cursor: pointer;
 }
 .create-task:hover {
-  background-color: coral;
+  background-color: rgb(158, 155, 155);
+}
+.input-task-name {
+  width: 100%;
+}
+.task {
+  position: relative;
+  width: auto;
+  height: 100px;
+  border-radius: 2px;
+  margin: 10px;
+  background-color: #ffffff5d;
+}
+.drop_zone {
+  height: 10px;
 }
 </style>
